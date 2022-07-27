d1793d8c8058f8295e76d41d9de57e86e6a908cb
tuanminh2 <aroi671620@gmail.com>
2022-07-27 10:55:21 +0700

    userController

--- nodeapp/src/config/connectDB.js
+++ nodeapp/src/config/connectDB.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize("sernapp", "root", null, {
   host: "localhost",
-  port: "3307",
+  port: "3306",
   dialect: "mysql"
 });
 let connectDB = async () => {
--- nodeapp/src/controllers/userController.js
+++ nodeapp/src/controllers/userController.js
@@ -40,6 +40,7 @@ let hdlCreateNewUser = async (req, res) => {
   return res.status(200).json(message);
 };
 module.exports = {
+  
   hdlLogin: hdlLogin,
   hdlGetAllUser: hdlGetAllUser,
   hdlCreateNewUser: hdlCreateNewUser,

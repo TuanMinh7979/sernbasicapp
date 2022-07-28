540c7d223dc1fe0f285d3bc8644d730f990df37b
tuanminh2 <aroi671620@gmail.com>
2022-07-28 17:00:43 +0700

    v49

--- reactapp/src/containers/HomePage/Section/Specialty.js
+++ reactapp/src/containers/HomePage/Section/Specialty.js
@@ -5,6 +5,7 @@ import "./Specialty.scss";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import specialtyImg from "../../../assets/images/star2.jpg";
 
 class Specialty extends Component {
   render() {
@@ -12,30 +13,41 @@ class Specialty extends Component {
       dots: false,
       infinite: true,
       speed: 500,
-      slidesToShow: 1,
+      slidesToShow: 4,
       slidesToScroll: 1,
     };
     return (
       <div className="section-specialty">
-        <div className="specialty-content">
+        <div className="specialty-container">
+          <div className="specialty-header">
+            <span className="title-section"> tieu de</span>
+            <button className="btn-section">xem them</button>
+          </div>
+
           <Slider {...settings}>
-            <div>
-              <h3>1</h3>
+            <div className="specialty-body">
+              <img src={specialtyImg} />
+              <h3>asdfasdf</h3>
             </div>
-            <div>
-              <h3>2</h3>
+            <div className="specialty-body">
+              <img src={specialtyImg} />
+              <h3>asdfasdf</h3>
             </div>
-            <div>
-              <h3>3</h3>
+            <div className="specialty-body">
+              <img src={specialtyImg} />
+              <h3>asdfasdf</h3>
             </div>
-            <div>
-              <h3>4</h3>
+            <div className="specialty-body">
+              <img src={specialtyImg} />
+              <h3>asdfasdf</h3>
             </div>
-            <div>
-              <h3>5</h3>
+            <div className="specialty-body">
+              <img src={specialtyImg} />
+              <h3>asdfasdf</h3>
             </div>
-            <div>
-              <h3>6</h3>
+            <div className="specialty-body">
+              <img src={specialtyImg} />
+              <h3>asdfasdf</h3>
             </div>
           </Slider>
         </div>

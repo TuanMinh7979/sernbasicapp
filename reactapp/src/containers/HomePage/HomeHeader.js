810d674ea1c3f9459f5bee0a4e6fdd152173fd1d
tuanminh2 <aroi671620@gmail.com>
2022-07-28 15:19:27 +0700

    v48

--- reactapp/src/containers/HomePage/HomeHeader.js
+++ reactapp/src/containers/HomePage/HomeHeader.js
@@ -2,10 +2,15 @@ import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import "./HomeHeader.scss";
-import {FormattedMessage} from "react-intl"
+import { FormattedMessage } from "react-intl";
+import { changeLanguageAction } from "../../store/actions";
 class HomeHeader extends Component {
+  hdlChangeLanguge = (language) => {
+    this.props.changeLanguageActionComMethod(language);
+  };
   render() {
-    console.log("check prop: ", this.prop)
+    let curlanguage = this.props.language;
+
     return (
       <React.Fragment>
         <div className="home-header-container">
@@ -17,43 +22,76 @@ class HomeHeader extends Component {
             <div className="center-content">
               <div className="child-content">
                 <div>
-                   <b><FormattedMessage id="homeheader.specialty"></FormattedMessage></b>
-                </div> 
-                <div className="sub-title"><FormattedMessage id="homeheader.searchdoctor"></FormattedMessage></div>
+                  <b>
+                    <FormattedMessage id="homeheader.specialty"></FormattedMessage>
+                  </b>
+                </div>
+                <div className="sub-title">
+                  <FormattedMessage id="homeheader.searchdoctor"></FormattedMessage>
+                </div>
               </div>
               <div className="child-content">
                 <div>
-                  <b><FormattedMessage id="homeheader.healthy-facility"></FormattedMessage></b>
+                  <b>
+                    <FormattedMessage id="homeheader.healthy-facility"></FormattedMessage>
+                  </b>
                 </div>
-                <div className="sub-title"><FormattedMessage id="homeheader.select-room"></FormattedMessage></div>
+                <div className="sub-title">
+                  <FormattedMessage id="homeheader.select-room"></FormattedMessage>
+                </div>
               </div>
               <div className="child-content">
                 <div>
-                  <b><FormattedMessage id="homeheader.doctor"></FormattedMessage></b>
+                  <b>
+                    <FormattedMessage id="homeheader.doctor"></FormattedMessage>
+                  </b>
                 </div>
-                <div className="sub-title"><FormattedMessage id="homeheader.select-doctor"></FormattedMessage></div>
+                <div className="sub-title">
+                  <FormattedMessage id="homeheader.select-doctor"></FormattedMessage>
+                </div>
               </div>
               <div className="child-content">
                 <div>
-                  <b><FormattedMessage id="homeheader.fee"></FormattedMessage></b>
+                  <b>
+                    <FormattedMessage id="homeheader.fee"></FormattedMessage>
+                  </b>
                 </div>
-                <div className="sub-title"><FormattedMessage id="homeheader.check-healthy"></FormattedMessage></div>
+                <div className="sub-title">
+                  <FormattedMessage id="homeheader.check-healthy"></FormattedMessage>
+                </div>
               </div>
             </div>
             <div className="right-content">
               <div className="support">
-                <i className="fas fa-question-circle"></i> <FormattedMessage id="homeheader.support"></FormattedMessage>
+                <i className="fas fa-question-circle"></i>{" "}
+                <FormattedMessage id="homeheader.support"></FormattedMessage>
               </div>
-              <div className="language-VN">VN</div>
+              <div
+                className={
+                  curlanguage == "vi" ? "language-vi active" : "language-vi"
+                }
+              >
+                <span onClick={() => this.hdlChangeLanguge("vi")}>VN</span>
+              </div>
               <div>|</div>
-              <div className="language-EN">EN</div>
+              <div
+                className={
+                  curlanguage == "en" ? "language-en active" : "language-en"
+                }
+              >
+                <span onClick={() => this.hdlChangeLanguge("en")}>EN</span>
+              </div>
             </div>
           </div>
         </div>
         <div className="home-header-banner">
           <div className="content-up">
-            <div className="title1"><FormattedMessage id="banner.title1"></FormattedMessage></div>
-            <div className="title2"><FormattedMessage id="banner.title2"></FormattedMessage></div>
+            <div className="title1">
+              <FormattedMessage id="banner.title1"></FormattedMessage>
+            </div>
+            <div className="title2">
+              <FormattedMessage id="banner.title2"></FormattedMessage>
+            </div>
             <div className="search">
               <i className="fas fa-search"></i>
               <input type="text" />
@@ -62,28 +100,52 @@ class HomeHeader extends Component {
           <div className="content-down">
             <div className="options">
               <div className="option-child">
-                <div className="icon-child"><i className="fas fa-home"></i></div>
-                <div className="text-child"><FormattedMessage id="banner.child1"></FormattedMessage></div>
+                <div className="icon-child">
+                  <i className="fas fa-home"></i>
+                </div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child1"></FormattedMessage>
+                </div>
               </div>
               <div className="option-child">
-                <div className="icon-child"><i className="fas fa-home"></i></div>
-                <div className="text-child"><FormattedMessage id="banner.child2"></FormattedMessage></div>
+                <div className="icon-child">
+                  <i className="fas fa-home"></i>
+                </div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child2"></FormattedMessage>
+                </div>
               </div>
               <div className="option-child">
-                <div className="icon-child"><i className="fas fa-home"></i></div>
-                <div className="text-child"><FormattedMessage id="banner.child3"></FormattedMessage></div>
+                <div className="icon-child">
+                  <i className="fas fa-home"></i>
+                </div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child3"></FormattedMessage>
+                </div>
               </div>
               <div className="option-child">
-                <div className="icon-child"><i className="fas fa-home"></i></div>
-                <div className="text-child"><FormattedMessage id="banner.child4"></FormattedMessage></div>
+                <div className="icon-child">
+                  <i className="fas fa-home"></i>
+                </div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child4"></FormattedMessage>
+                </div>
               </div>
               <div className="option-child">
-                <div className="icon-child"><i className="fas fa-home"></i></div>
-                <div className="text-child"><FormattedMessage id="banner.child5"></FormattedMessage></div>
+                <div className="icon-child">
+                  <i className="fas fa-home"></i>
+                </div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child5"></FormattedMessage>
+                </div>
               </div>
               <div className="option-child">
-                <div className="icon-child"><i className="fas fa-home"></i></div>
-                <div className="text-child"><FormattedMessage id="banner.child6"></FormattedMessage></div>
+                <div className="icon-child">
+                  <i className="fas fa-home"></i>
+                </div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child6"></FormattedMessage>
+                </div>
               </div>
             </div>
           </div>
@@ -93,17 +155,24 @@ class HomeHeader extends Component {
   }
 }
 
+// Mỗi khi store update mapStateToProps sẽ được gọi, object mà nó trả về sẽ được merge với props của container.
 const mapStateToProps = (state) => {
   return {
     isLoggedIn: state.user.isLoggedIn,
-    lang: state.app.languge,
-    //inject
-
+    language: state.app.language,
+    //user va app tu rootreducer(tong hop cac reducer)
   };
 };
 
+//map cac action method da dang ky voi reducers voi method cua component , de component nay dispatch(actionMethod)
 const mapDispatchToProps = (dispatch) => {
-  return {};
+  return {
+    changeLanguageActionComMethod: (language) =>
+      //call arrow function cua action
+      //tuc la map changeLanguageActionComMethod method cua component hien tai
+      //voi changeLanguageAction() method cua appActions
+      dispatch(changeLanguageAction(language)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeHeader);
--- reactapp/src/store/actions/appActions.js
+++ reactapp/src/store/actions/appActions.js
@@ -1,10 +1,18 @@
-import actionTypes from './actionTypes';
+import { languages } from "../../utils";
+import actionTypes from "./actionTypes";
 
 export const appStartUpComplete = () => ({
-    type: actionTypes.APP_START_UP_COMPLETE
+  type: actionTypes.APP_START_UP_COMPLETE,
 });
 
 export const setContentOfConfirmModal = (contentOfConfirmModal) => ({
-    type: actionTypes.SET_CONTENT_OF_CONFIRM_MODAL,
-    contentOfConfirmModal: contentOfConfirmModal
+  type: actionTypes.SET_CONTENT_OF_CONFIRM_MODAL,
+  contentOfConfirmModal: contentOfConfirmModal,
+});
+
+//changeLanguageAction la mot function (la 2 arrow function)
+//return 1obj : key la type(required)
+export const changeLanguageAction = (language) => ({
+  type: actionTypes.CHANGE_LANGUAGE,
+  language: language,
 });
